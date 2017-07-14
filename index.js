865e783cbcf4887a8fa798e5c45434a8f4ce3653
Flatulentwalrus <levelupit@outlook.com>
2017-07-14 10:38:12 +0100

    added mlg bot

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ bot.on("message", async message => {
     if(message.author == bot.user) return;
     var input = message.toString();
     input = input.toLowerCase();
-    if(input.indexOf("gitgud") != -1 || input.indexOf("git gud") != -1 || input.indexOf("dark souls") != -1){
-          console.log('We got here');
-         message.reply("https://media.tenor.com/images/628f092d12cce4af0e12ff4e94137730/tenor.gif");
-    }
+    if(input.indexOf("gitgud") != -1 || input.indexOf("git gud") != -1 || input.indexOf("dark souls") != -1) message.reply("https://media.tenor.com/images/628f092d12cce4af0e12ff4e94137730/tenor.gif");
+    if(input.indexOf("mlg") != -1)  message.reply("http://i1.kym-cdn.com/photos/images/newsfeed/000/793/372/fe7.gif");
 });
 bot.login('MzMzOTc1Mjk1MTM0NzkzNzI4.DEUl1g.VtpsiFsO1eyPP1fYHxlU5JnkeRs');
